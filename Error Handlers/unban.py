6f8165ca505306fdf7ad16a396ac8ba7a732e7a6
FoUnDeRR <88034097+FoUnDeRR@users.noreply.github.com>
2021-08-03 12:46:16 +0530

    Update unban.py

--- Error Handlers/unban.py
+++ Error Handlers/unban.py
@@ -1,9 +1,3 @@
-@client.command()
-@commands.has_permissions(administrator=True)
-async def unban(ctx, member : discord.Member, reason=None):
-    await member.unban(reason=reason)
-    await ctx.send(f'{member} was unbanned!', delete_after=5)
-
 @unban.error
 async def unban_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
