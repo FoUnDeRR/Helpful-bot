254074735d966fc6d4ed5185c00c6789c6a71ab4
FoUnDeRR <88034097+FoUnDeRR@users.noreply.github.com>
2021-08-03 11:18:21 +0530

    Create say.py

---
+++ Basic Command/say.py
@@ -0,0 +1,5 @@
+@client.command()
+async def hello(ctx):
+  await ctx.send("Hellow there!")
+  
+#This is how you get a responses from your bot. All you have to do after putting this, type <bot prefix>hello Example:- >hello
