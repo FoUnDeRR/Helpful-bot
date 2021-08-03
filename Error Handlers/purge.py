559f00f7e1ad0f062a5c1d5e86a33f3c5ccc414b
FoUnDeRR <88034097+FoUnDeRR@users.noreply.github.com>
2021-08-03 12:45:51 +0530

    Update purge.py

--- Error Handlers/purge.py
+++ Error Handlers/purge.py
@@ -1,10 +1,3 @@
-@client.command(pass_context=True)
-@commands.has_permissions(manage_messages=True)
-async def purge(ctx, limit: int):
-        await ctx.channel.purge(limit=limit)
-        await ctx.send('Chat was purged by {}'.format(ctx.author.mention), delete_after=5)
-        await ctx.message.delete()
-
 @purge.error
 async def purge_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
