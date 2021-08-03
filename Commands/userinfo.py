6b3e1105afa0d8de24ba79cb3717135fee5b0894
FoUnDeRR <88034097+FoUnDeRR@users.noreply.github.com>
2021-08-03 13:08:03 +0530

    Create userinfo.py

---
+++ Commands/userinfo.py
@@ -0,0 +1,20 @@
+@client.command(aliases=['userinformation'])
+async def userinfo(ctx, member: discord.Member = None):
+    if not member:  # if member is no mentioned
+        member = ctx.message.author  # set member as the author
+    roles = [role for role in member.roles]
+    embed = discord.Embed(colour=discord.Colour.purple(), timestamp=ctx.message.created_at,
+                          title=f"User Info - {member}")
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_footer(text=f"Requested by {ctx.author}")
+
+    embed.add_field(name="User ID:", value=member.id)
+    embed.add_field(name="Discord Name:", value=member.display_name)
+
+    embed.add_field(name="Created Account On:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+    embed.add_field(name="Joined Server On:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+
+    embed.add_field(name="Roles:", value="".join([role.mention for role in roles]))
+    embed.add_field(name="Highest Role:", value=member.top_role.mention)
+    print(member.top_role.mention)
+    await ctx.send(embed=embed)
