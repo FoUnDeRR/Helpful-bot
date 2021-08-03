bc824d956b40510d1736f9d351453da109ddf21c
FoUnDeRR <88034097+FoUnDeRR@users.noreply.github.com>
2021-08-03 13:10:46 +0530

    Create meme.py

---
+++ Commands/meme.py
@@ -0,0 +1,26 @@
+@client.command(pass_context=True)
+async def meme(ctx):
+    embed = discord.Embed(title="", description="")
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            embed.set_footer(text =f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+
+
+def convert(time):
+  pos = ["s","m","h","d"]
+
+  time_dict = {"s" : 1, "m" : 60, "h" : 3600, "d": 3600*24}
+
+  unit = time[-1]
+
+  if unit not in pos:
+    return -1
+  try:
+    val = int(time[:-1])
+  except:
+    return -2
+
+  return val * time_dict[unit]
