bd8139b34b34ddb45af53cef71d8af6af4fb6cc0
FoUnDeRR <88034097+FoUnDeRR@users.noreply.github.com>
2021-08-03 12:44:58 +0530

    Update ban.py

--- Error Handlers/ban.py
+++ Error Handlers/ban.py
@@ -1,9 +1,3 @@
-@client.command(aliases=['b'])
-@commands.has_permissions(ban_members = True)
-async def ban(ctx,member : discord.Member,*,reason= "**No reason provided**"):
-    await ctx.send(member.name + "You have been banned from the Bots Tryout Server, Because:"+reason)
-    await member.ban(reason=reason)
-
 @ban.error
 async def ban_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
