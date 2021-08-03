34668f4b908177632408676ce0b0afb3e95e39d2
FoUnDeRR <88034097+FoUnDeRR@users.noreply.github.com>
2021-08-03 11:13:10 +0530

    Create calculator.py
    This is how you make a calculator with buttons!

---
+++ Commands/calculator.py
@@ -0,0 +1,71 @@
+buttons = [
+    [
+        Button(style=ButtonStyle.grey, label='1'),
+        Button(style=ButtonStyle.grey, label='2'),
+        Button(style=ButtonStyle.grey, label='3'),
+        Button(style=ButtonStyle.blue, label='×'),
+        Button(style=ButtonStyle.red, label='Exit')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='4'),
+        Button(style=ButtonStyle.grey, label='5'),
+        Button(style=ButtonStyle.grey, label='6'),
+        Button(style=ButtonStyle.blue, label='÷'),
+        Button(style=ButtonStyle.red, label='←')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='7'),
+        Button(style=ButtonStyle.grey, label='8'),
+        Button(style=ButtonStyle.grey, label='9'),
+        Button(style=ButtonStyle.blue, label='+'),
+        Button(style=ButtonStyle.red, label='Clear')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='00'),
+        Button(style=ButtonStyle.grey, label='0'),
+        Button(style=ButtonStyle.grey, label='.'),
+        Button(style=ButtonStyle.blue, label='-'),
+        Button(style=ButtonStyle.green, label='=')
+    ],
+]
+ 
+#calculates answer
+def calculate(exp):
+    o = exp.replace('×', '*')
+    o = o.replace('÷', '/')
+    result = ''
+    try:
+        result = str(eval(o))
+    except:
+        result = 'An error occurred.'
+    return result
+ 
+@client.command()
+async def calc(ctx):
+    m = await ctx.send(content='**__Loading Calculators...__**')
+    expression = 'None'
+    delta = datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
+    e = discord.Embed(title=f'{ctx.author.name}\'s calculator | {ctx.author.id}', description=expression,
+                        timestamp=delta)
+    await m.edit(components=buttons, embed=e)
+    while m.created_at < delta:
+        res = await client.wait_for('button_click')
+        if res.author.id == int(res.message.embeds[0].title.split('|')[1]) and res.message.embeds[
+            0].timestamp < delta:
+            expression = res.message.embeds[0].description
+            if expression == 'None' or expression == 'An error occurred.':
+                expression = ''
+            if res.component.label == 'Exit':
+                await res.respond(content='**__Calculator Closed__**', type=7)
+                break
+            elif res.component.label == '←':
+                expression = expression[:-1]
+            elif res.component.label == 'Clear':
+                expression = 'None'
+            elif res.component.label == '=':
+                expression = calculate(expression)
+            else:
+                expression += res.component.label
+            f = discord.Embed(title=f'{res.author.name}\'s calculator|{res.author.id}', description=expression,
+                                timestamp=delta)
+            await res.respond(content='', embed=f, components=buttons, type=7)
